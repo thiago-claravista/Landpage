293214be22fae606403d74d474e0c3e7f3893e4f
Martins Pereira <martins.silva@etec.sp.gov.br>
2020-10-12 03:29:36 -0300

    primeiro commit

---
+++ src/sections/Navigation/utility/data.js
@@ -0,0 +1,34 @@
+const Data = {
+  menuItems: [
+    {
+      name: "Home",
+      path: "__next",
+    },
+    {
+      name: "Service",
+      path: "service",
+      offset: "-120"
+    },
+    {
+      name: "Token",
+      path: "token",
+      offset: "-100"
+    },
+    {
+      name: "About",
+      path: "about",
+      offset: "-100"
+    },
+    {
+      name: "FAQ",
+      path: "faq",
+      offset: "-60"
+    },
+    {
+      name: "Contact",
+      path: "contact",
+      offset: "-70"
+    }
+  ]
+};
+export default Data;
